63913829e0f85aa206db293470bf377f00147b8c
lais-vaz <lais.s.vaz@aluno.senai.br>
2026-08-26 15:10:24 -0300

    pasta inteira
    Subindo todo o código para a correção

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import PlaylistMusic from './src/screens/PlaylistMusic';
+
+export default function App() {
+  return (
+    <>
+      <PlaylistMusic />
+      <StatusBar style="auto" />
+    </>
+  );
+}
